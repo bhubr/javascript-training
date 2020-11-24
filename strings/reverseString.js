beefe95cc09df41d042177ef3416c139ab75ccd7
Benoît Hubert <benoithubert@gmail.com>
2020-11-24 08:08:54 +0100

    Add reverseString exercise

---
+++ strings/reverseString.js
@@ -0,0 +1,18 @@
+// strings/reverseString.js
+const assert = require('assert');
+
+/*--------------*
+ * INSTRUCTIONS |
+ *--------------*
+ Ecrire une fonction `reverseString` qui accepte comme paramètre une chaîne de caractères, et renvoie cette chaîne inversée.
+
+Ainsi, si on passe paramètre `hello`, elle renverra `olleh`.
+*/
+
+// ECRIS TON CODE ICI
+
+// TESTS : NE RIEN CHANGER SOUS CETTE LIGNE !
+assert.strictEqual(reverseString('hello'), 'olleh');
+assert.strictEqual(reverseString('JavaScript'), 'tpircSavaJ');
+assert.strictEqual(reverseString('I love code'), 'edoc evol I');
+console.log('✅ reverseString : test passé !');
